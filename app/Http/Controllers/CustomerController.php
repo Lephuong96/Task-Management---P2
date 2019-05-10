28fc9d440a76365d8763036e8058e322a915d388
Lephuong96 <bigankroi@gmail.com>
2019-05-10 14:01:56 +0700

    first commit

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    public function index(){
+        return view('modules.customer.index');
+    }
+    public function create(){
+        echo "Hiển thị form tạo khách hàng";
+    }
+    public function store(Request $request){
+
+    }
+    public function show($id){
+
+    }
+    public function edit($id){
+
+    }
+    public function update(Request $request, $id){
+
+    }
+    public function destroy($id){
+
+    }
+
+
+}
---
+++ resources/views/modules/customer/index.blade.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Danh sách khách hàng</title>
+</head>
+<body>
+<h1>Danh sách khách hàng</h1>
+<table border="1">
+    <thead>
+    <tr>
+        <th>STT</th>
+        <th>Họ và tên</th>
+        <th>Số điện thoại</th>
+        <th>Email</th>
+        <th>Thao tác</th>
+    </tr>
+    </thead>
+    <thead>
+    <tr>
+        <th>1</th>
+        <th>Triệu Văn Trác</th>
+        <th>0987731735</th>
+        <th>abcg@gmail.com</th>
+        <th><a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a&</th>
+    </tr>
+    </thead>
+    <thead>
+    <tr>
+        <th>2</th>
+        <th>Lý Á Bằng</th>
+        <th>0827232233</th>
+        <th>hdye@hajud.com</th>
+        <th><a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a&</th>
+    </tr>
+    </thead>
+    <thead>
+    <tr>
+        <th>3</th>
+        <th>Lý Văn Cơ</th>
+        <th>0837648395</th>
+        <th>chsyen@hdsyd.com</th>
+        <th><a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a&</th>
+    </tr>
+    </thead>
+    <thead>
+    <tr>
+        <th>4</th>
+        <th>Hoàng Kim Bảng</th>
+        <th>0938742744</th>
+        <th>atssj@gmail.com</th>
+        <th><a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a&</th>
+    </tr>
+    </thead>
+    <thead>
+    <tr>
+        <th>5</th>
+        <th>Hoàng Mộng Giao</th>
+        <th>087467381</th>
+        <th>lsidjsdy@gmail.com</th>
+        <th><a href="#">Xem</a> | <a href="#">Sửa</a> | <a href="#">Xóa</a&</th>
+    </tr>
+    </thead>
+
+
+</table>
+</body>
+</html>
